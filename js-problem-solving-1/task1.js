d08e5cc2242f2461c3c2e8b3f8b707e4a4fc3144
naeemul-online <naeemul.online@gmail.com>
2024-01-26 07:44:07 +0300

    task 1 and task 2 solved

---
+++ js-problem-solving-1/task1.js
@@ -0,0 +1,9 @@
+// Task: 1
+/* My money */
+var myMoney = 1000;
+
+/* cost of fruits */
+var costOfFruits = 700;
+
+/* Return from shopkeeper */
+console.log(myMoney - costOfFruits);
---
+++ js-problem-solving-1/task2.js
@@ -0,0 +1,11 @@
+// marks of five subjects
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalNumber = math + biology + chemistry + physics + bangla;
+var averageNumber = totalNumber / 5;
+
+
+console.log(averageNumber.toFixed(2))
